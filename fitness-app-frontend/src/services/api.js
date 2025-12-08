4b2b8464746c7286a65d1c69cdcc68e63b9a26d6
Kulkant <kulkantsharma9883@gmail.com>
2025-12-08 22:04:13 +0530

    Changed baseUrl in frotend service api

--- fitness-app-frontend/src/services/api.js
+++ fitness-app-frontend/src/services/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL:
+    process.env.NODE_ENV === `production`
+      ? `https://your-backend.onrender.com/api`
+      : `http://localhost:5000/api`,
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
